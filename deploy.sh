27c0ca25885e0f3ac55d778717bf64e9833419b8
nobuti <buti@nobuti.com>
2015-11-28 13:50:22 +0100

    Deploy 28-11-15 13:50:22

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 # Script deploy to heroku
 DEPLOYDATE=`date +"%d-%m-%y %T"`
-git pull heroku master
 git add -A
 git commit -m "Deploy $DEPLOYDATE"
 git push
+git pull heroku master
 git push heroku master
 heroku open
